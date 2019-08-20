700be76e94b7a70442ec96fa8cf751bdcd400e1a
zongzeliunt <zongzeli2@my.unt.edu>
2019-08-20 15:03:35 -0600

    add bin_opt

---
+++ function/bin_opt.py
@@ -0,0 +1,32 @@
+
+print bin(6)
+
+print int("0b11101", 2)
+
+new = 6<<8
+
+print bin(new)
+print new
+
+new += 3
+print new
+print bin(new)
+
+print bin(12)
+
+print new | 12
+print bin(new | 12)
+
+
+
+
+
+
+
+
+
+
+
+IDB = (6 << 8) | 12
+print IDB
+print bin(IDB)
