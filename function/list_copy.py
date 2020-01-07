e7eae2e8b6fd7ce806e25594542fb8e66b98a564
Zongze Li <zongze.li@cern.ch>
2020-01-07 15:26:40 -0600

    add list copy case

--- function/list_copy.py
+++ function/list_copy.py
@@ -4,6 +4,7 @@ list_1 = [1,2,3]
 
 list_2 = list_1
 list_3 = copy.copy(list_1)
+list_4 = list_1[:]
 #python 3 can use list_1.copy()
 
 del(list_2[2])
@@ -12,5 +13,10 @@ print list_1
 print list_2
 #both are [1,2], so list_2 is a pointer pointing to list_1
 
+del(list_3[0])
+
 print list_3
-#[1,2,3] list_3 is copied from list_1
+#[2,3] list_3 is copied from list_1 but have its own memory
+
+print list_4
+#[1,2,3] list_4 is copied from list_1
