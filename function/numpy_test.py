df060e60cd919ca440787391515fecf5568530dc
zongzeliunt <zongzeli2@my.unt.edu>
2020-02-03 19:59:58 -0600

    change numpy test

--- function/numpy_test.py
+++ function/numpy_test.py
@@ -3,78 +3,84 @@ import copy, numpy as np
 a = np.zeros ((2,3), int)
 b = np.zeros ((2,3), int)
 
-print "initial a"
-print a
-print "initial b"
-print b
-print "type of a"
-print type(a)
+print ("initial a")
+print (a)
+print ("initial b")
+print (b)
+print ("type of a")
+print (type(a))
 
 #a[0] = [1,2,3]
 #a[1] = [4,5,6]
 a[:][:] = [[1,2,3],[4,5,6]]
 b[:][:] = [[7,8,9],[10,11,12]]
 
-print "a with value"
-print a
-print "b with value"
-print b
+print ("a with value")
+print (a)
+print ("b with value")
+print (b)
 
 c = a + b
-print "c = a + b"
-print c
+print ("c = a + b")
+print (c)
 d = a*b
-print "d = a * b"
-print d
+print ("d = a * b")
+print (d)
 
 g= a.dot(b.T)
-print "g = a.dot(b.T)"
-print g
+print ("g = a.dot(b.T)")
+print (g)
 
-print "==================================="
-print "second test"
+print ("===================================")
+print ("second test")
 a = np.zeros( (1,3), int)
 a[0] = [1,2,3]
 
 b = np.zeros( (1,3), int)
 b[0] = [4,5,6]
 
-print "a with value"
-print a
-print "b with value"
-print b
+print ("a with value")
+print (a)
+print ("b with value")
+print (b)
 
 c = a+ b
-print "c = a + b"
-print c
+print ("c = a + b")
+print (c)
 
 d = a*b
-print "d = a * b"
-print d
+print ("d = a * b")
+print (d)
 
 e = a.T * b
-print "e = a.T * b" 
-print e
+print ("e = a.T * b")
+print (e)
 
 f = a * b.T
-print "f = a * b.T"
-print f
+print ("f = a * b.T")
+print (f)
 
 g= a.dot(b.T)
-print "g = a.dot(b.T)"
-print g
+print ("g = a.dot(b.T)")
+print (g)
 
-print "==================================="
-print "third test"
+print ("===================================")
+print ("third test")
 a = np.zeros( (1,3), int)
 a[0] = [1,2,3]
 
 b = np.tile(a, 2)
-print "a tile 2"
-print b
+print ("a tile 2")
+print (b)
 
 c = np.tile(a, (2,2))
-print "a tile 2 * 2"
-print c
+print ("a tile 2 * 2")
+print (c)
+
+print ("===================================")
+print ("nonzero")
+x = np.array([[3, 0, 0], [0, 4, 0], [5, 6, 7]])
+print(np.nonzero(x))
+
 
 
