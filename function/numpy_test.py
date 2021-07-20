926f7cf0862ea443794f5ab18fc268bd5e690f00
Zongze Li <zongze.li@cern.ch>
2021-07-20 00:46:19 -0700

    update numpy test

--- function/numpy_test.py
+++ function/numpy_test.py
@@ -1,7 +1,10 @@
 import copy, numpy as np
 
+print ("===================================")
+print ("zero test, np array")
+#{{{
 a = np.zeros ((2,3), int)
-b = np.zeros ((2,3), int)
+b = np.zeros_like (a)
 
 print ("initial a")
 print (a)
@@ -9,17 +12,28 @@ print ("initial b")
 print (b)
 print ("type of a")
 print (type(a))
+print ("size of a")
+print (a.size)
+print ("shape of a")
+print (a.shape)
 
-#a[0] = [1,2,3]
-#a[1] = [4,5,6]
-a[:][:] = [[1,2,3],[4,5,6]]
+#a[:][:] = [[1,2,3],[4,5,6]]
+
+print ("random whole a, assign whole b, assign a[0]")
+a = np.random.randint(10, size = a.shape)
+a[0] = [1,2,3]
 b[:][:] = [[7,8,9],[10,11,12]]
 
-print ("a with value")
+print ("a value")
 print (a)
-print ("b with value")
+print ("b value")
 print (b)
+#}}}
 
+"""
+print ("===================================")
+print ("first test, basic operations")
+#{{{
 c = a + b
 print ("c = a + b")
 print (c)
@@ -27,48 +41,20 @@ d = a*b
 print ("d = a * b")
 print (d)
 
-g= a.dot(b.T)
-print ("g = a.dot(b.T)")
-print (g)
-
-print ("===================================")
-print ("second test")
-a = np.zeros( (1,3), int)
-a[0] = [1,2,3]
-
-b = np.zeros( (1,3), int)
-b[0] = [4,5,6]
-
-print ("a with value")
-print (a)
-print ("b with value")
-print (b)
-
-c = a+ b
-print ("c = a + b")
-print (c)
-
-d = a*b
-print ("d = a * b")
-print (d)
-
-e = a.T * b
-print ("e = a.T * b")
+e = a.dot(b.T)
+print ("e = a.dot(b.T)")
 print (e)
 
-f = a * b.T
-print ("f = a * b.T")
+f = a.T.dot(b)
+print ("f = a.T.dot(b)")
 print (f)
+#}}}
+"""
 
-g= a.dot(b.T)
-print ("g = a.dot(b.T)")
-print (g)
-
+"""
 print ("===================================")
-print ("third test")
-a = np.zeros( (1,3), int)
-a[0] = [1,2,3]
-
+print ("second test, tile 就是同一个矩阵复制")
+#{{{
 b = np.tile(a, 2)
 print ("a tile 2")
 print (b)
@@ -77,10 +63,34 @@ c = np.tile(a, (2,2))
 print ("a tile 2 * 2")
 print (c)
 
+d = np.tile(a, (2,1))
+print ("a tile 1 * 2")
+print (d)
+#}}}
+"""
+
 print ("===================================")
-print ("nonzero")
+print ("third test, nonzero")
+#{{{
 x = np.array([[3, 0, 0], [0, 4, 0], [5, 6, 7]])
-print(np.nonzero(x))
+b = np.nonzero(x)
+print(np.array(b).ndim)
+    #2
+print(b)
+    #(array([0, 1, 2, 2, 2]), array([0, 1, 0, 1, 2]))
+    #即，(0，0),(1,1),(2,0),(2,1),(2,2) 诡异的排列法
+    #但是其实这么排出来也是有道理，因为x可能是个多维的数组，b 的ndim就是把这个维告诉你，然后按每一维为单位打出非0的数的坐标。
+
+print(np.transpose(np.nonzero(x)))
+
+
+#}}}
+
+print ("===================================")
+print ("third test, random")
+#{{{
 
 
 
+
+#}}}
